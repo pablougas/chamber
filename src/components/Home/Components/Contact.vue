67c875eff61813ee82902ae0d43ab0001fe593f4
Pablo Ugas <pablo.ugas@gmail.com>
2018-07-20 16:03:26 -0700

    emailing

--- src/components/Home/Components/Contact.vue
+++ src/components/Home/Components/Contact.vue
@@ -1,12 +1,26 @@
 <template>
-  <v-container>
+  <v-container fluid>
 
   </v-container>
 </template>
 
 <script>
+import sgMail from '@sendgrid/mail';
+
 export default {
   name: 'Contact',
+  mounted () {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+      to: 'pablo@sky9.us',
+      from: 'test@example.com',
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere',
+      html: '<strong>even with Vue.js</strong>',
+    };
+    sgMail.send(msg)
+      .then(res => console.log(res))
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,23 +1,25 @@
-import '@babel/polyfill'
+import '@babel/polyfill';
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App'
-import router from './router'
-import VueTyperPlugin from 'vue-typer'
-import '../node_modules/slick-carousel/slick/slick.css'
-import '../node_modules/slick-carousel/slick/slick-theme.css'
+import sgMail from '@sendgrid/mail';
+import VueTyperPlugin from 'vue-typer';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import './plugins/vuetify';
+import '../node_modules/slick-carousel/slick/slick.css';
+import '../node_modules/slick-carousel/slick/slick-theme.css';
 
+Vue.use(sgMail);
+Vue.use(VueTyperPlugin);
 
-Vue.use(VueTyperPlugin)
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+});
